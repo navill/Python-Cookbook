fb3087b2e7563691fd30d75426d9808bc7101d4d
jihoon <blue_jihoon@naver.com>
2019-12-04 21:48:41 +0900

    first commit

--- chapter1_datastructure_algorithm/1.8_cal_dict.py
+++ chapter1_datastructure_algorithm/1.8_cal_dict.py
@@ -5,9 +5,26 @@ prices = {
 
 """
 dictionary에 있는 데이터에 대한 연산(min, max, sort 등...)이 필요할 경우
+zip()은 단 한번만 소비할 수 있는 이터레이터 생성 
 
+prices_and_names = zip(prices.values(), prices.keys())
+print(min(prices_and_names)) -> 정상 출력(zip 소모)
+print(max(prices_and_names)) -> ValueError: max의 인자가 비어있음
+
+가장 낮은 가격을 가지고 있는 주식은?
+# 일반적인 방법
+min(prices, key=lambda k: prices[k])  # FB 리턴
+min_value = prices(min(prices, key=lambda k: prices[k]))  # FB의 값(최소) 리턴
+# zip을 이용한 방법
+prices_and_names = zip(prices.values(), prices.keys())
+
+여러 엔트리가 동일한 값을 가질 경우 자동으로 키를 비교하여 정렬한다.
+>>> prices = { 'AAA' : 45.23, 'ZZZ': 45.23 } 
+>>> min(zip(prices.values(), prices.keys())) 
+(45.23, 'AAA')
+>>> max(zip(prices.values(), prices.keys())) 
+(45.23, 'ZZZ')
 """
-
 
 # 최소값
 min_price = min(zip(prices.values(), prices.keys()))
